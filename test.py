1dbf9c343e6b849070700c89a9fa37e0576dfe6a
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2020-03-08 14:21:19 +0530

    test.py: create pdf, identifiers code n comment and write in deff colors

--- test.py
+++ test.py
@@ -1,4 +1,24 @@
 import os
+from fpdf import FPDF
+
+# fpdf.add_link()
+pdf = FPDF()
+
+# This is for header and footer
+class PDF(FPDF):
+    def header(self):
+        # Select black for header
+        pdf.set_text_color(100,0,0)
+        # Select Arial bold 15
+        self.set_font('Arial', 'B', 15)
+        # Move to the right
+        self.cell(80)
+        # Framed title
+        self.cell(30, 10, f[1:], 'C')
+        # Line break
+        self.ln(20)
+
+pdf = PDF()
 
 file_path = []
 
@@ -12,9 +32,29 @@ for dir in os.listdir('code_file'):
 for files in file_path: 
     prg_file = open(files)
     for f in prg_file:
+        pdf.add_page()
+        break
+    
+    
+    for f in prg_file:
         if f[0] == "#":
-            print("Its comment          ",f)
+            f = f[1:]
+            if ("http" in f):
+                print( pdf.set_link(f, y = -1, page = -1))
+                print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",f)
+                pdf.cell(200, 8,txt=f, ln=1)
+            else:
+                pdf.set_text_color(0,70,0)
+                pdf.set_font("Arial", size=8)
+                pdf.cell(200, 8,txt=f, ln=1)
+                print("Its comment          ",f)
         else:
-            print("Its code             ",f)
-        
+            if (len(f) != 1):
+                f = ">>>    "+f
+                pdf.set_text_color(0,0,0)
+                pdf.set_font("Arial", size=8)
+                pdf.cell(200, 6, txt=f, ln=1)
+                print("Its code             ",f)
+
+pdf.output("simple_demo.pdf")      
     
